dd377e7c1a3a52129c38447c59d1fe1a084f21f1
DESKTOP-UFE7UMD\Anna <anuta.gomes@gmail.com>
2021-10-02 22:04:31 +0300

    feedparser.py -> rss_parser.py

--- news_agg/mainapp/rss_parser.py
+++ news_agg/mainapp/rss_parser.py
@@ -6,5 +6,8 @@ from bs4 import BeautifulSoup
 rss_url = 'https://112ua.tv/rsslist'
 rss_text = requests.get(rss_url).text
 soup = BeautifulSoup(rss_text, 'html.parser')
-list_ = soup.find('div', class_='statpage-content')
-print(list_)
+
+for a in soup.find('div', class_='statpage-content').findChildren('a')[:1]:
+    rss_link = a['href']
+    parser = feedparser.parse(rss_link)
+    print(parser.feed)
