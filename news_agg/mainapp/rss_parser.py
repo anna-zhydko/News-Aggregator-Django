d6b49a3ec786367794979ff48035c70d62d3d671
DESKTOP-UFE7UMD\Anna <anuta.gomes@gmail.com>
2021-10-05 09:52:01 +0300

    news display

--- news_agg/mainapp/rss_parser.py
+++ news_agg/mainapp/rss_parser.py
@@ -8,59 +8,68 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 from fuzzywuzzy import fuzz
 
-c = time.time()
 
-google_trends_url = 'https://trends.google.com/trends/trendingsearches/daily?geo=UA'
-path = 'C:\Program Files (x86)\chromedriver'
-driver = webdriver.Chrome(path)
-google_trends = []
+def get_trends():
+    c = time.time()
 
-driver.get(google_trends_url)
+    google_trends_url = 'https://trends.google.com/trends/trendingsearches/daily?geo=UA'
+    path = 'C:\Program Files (x86)\chromedriver'
+    driver = webdriver.Chrome(path)
+    google_trends = []
 
-try:
-    # Wait until all elements are loaded
-    main = WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located((By.CLASS_NAME, "generic-container-wrapper"))
-    )
-    date_count = 0
+    driver.get(google_trends_url)
 
-    # Loading all 7days-records
-    while date_count < 7:
-        date_count = len(driver.find_elements_by_class_name('content-header-title'))
-        # Wait until more button is loaded
+    try:
+        # Wait until all elements are loaded
         main = WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.CLASS_NAME, "feed-load-more-button"))
+            EC.presence_of_all_elements_located((By.CLASS_NAME, "generic-container-wrapper"))
         )
-        driver.find_element_by_class_name('feed-load-more-button').click()
+        date_count = 0
 
-    # Collecting information from 7days-records
-    for item in driver.find_elements_by_class_name('feed-item-header'):
-        item_parts = item.find_elements_by_tag_name('a')
-        title, subtitile = item_parts[0].text, item_parts[1].text
-        google_trends.append({'title': title, 'subtitle': subtitile})
-finally:
-    driver.quit()
+        # Loading all 7days-records
+        while date_count < 7:
+            date_count = len(driver.find_elements_by_class_name('content-header-title'))
+            # Wait until more button is loaded
+            main = WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME, "feed-load-more-button"))
+            )
+            driver.find_element_by_class_name('feed-load-more-button').click()
 
-rss_url = 'https://112ua.tv/rsslist'
-rss_text = requests.get(rss_url).text
-soup = BeautifulSoup(rss_text, 'html.parser')
-possible_trends = []  # the list stores google trends that could belong to the rss record
+        # Collecting information from 7days-records
+        for item in driver.find_elements_by_class_name('feed-item-header'):
+            item_parts = item.find_elements_by_tag_name('a')
+            title, subtitile = item_parts[0].text, item_parts[1].text
+            google_trends.append({'title': title, 'subtitle': subtitile})
+    finally:
+        driver.quit()
 
-# Getting records from all 112 rss lists
-for a in soup.find('div', class_='statpage-content').findChildren('a'):
-    rss_link = a['href']
-    parser = feedparser.parse(rss_link)  # feedparser for rss
-    for entry in parser.entries:
-        title, link, fulltext, published = entry.title, entry.link, entry.fulltext[:100], entry.published
+    rss_url = 'https://112ua.tv/rsslist'
+    rss_text = requests.get(rss_url).text
+    soup = BeautifulSoup(rss_text, 'html.parser')
+    trends_112 = []  # the list stores all 112 news that has been matched with google trends
 
-        # Comparing rss-record with google trend
-        for trend in google_trends:
-            match = fuzz.token_sort_ratio(trend['title'] + trend['subtitle'], title + fulltext)
-            if match > 50:  # match should be more than 50%
-                google_trends.append(trend['title'])
-                # print('112', title.lower() + fulltext)
-                # print('trend', trend['title'].lower() + trend['subtitle'].lower())
-                # print('match', match)
+    # Getting records from all 112 rss lists
+    for a in soup.find('div', class_='statpage-content').findChildren('a'):
+        rss_link = a['href']
+        parser = feedparser.parse(rss_link)  # feedparser for rss
+        for entry in parser.entries:
+            possible_trends = []  # the list stores google trends that could belong to the rss record
+            title, link, fulltext, published = entry.title, entry.link, entry.fulltext[:100], entry.published
+
+            # Comparing rss-record with google trend
+            for trend in google_trends:
+                match = fuzz.token_sort_ratio(trend['title'] + trend['subtitle'], title + fulltext)
+                if match > 50:  # match should be more than 50%
+                    possible_trends.append(trend['title'])
+
+            # if 112_news has been matched at least one google trend
+            if possible_trends:
+                trends_112.append({'title': title, 'link': link, 'published': published,
+                                   'google_trends': possible_trends})
+    print(trends_112)
+    print(time.time() - c)
+    return trends_112
 
 
-print(time.time() - c)
+if __name__ == "__main__":
+    get_trends()
--- news_agg/mainapp/views.py
+++ news_agg/mainapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from mainapp.rss_parser import get_trends
+import time
 
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    return render(request, 'mainapp/index.html', {'trends_112': get_trends()})
